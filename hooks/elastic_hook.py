f43f8369cc102b3de48b38f2337e76e5a1952abc
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2017-05-10 16:49:40 +0800

    Implement Airflow hook for ElasticSearch

---
+++ hooks/elastic_hook.py
@@ -0,0 +1,25 @@
+import logging
+
+import requests
+from airflow import AirflowException
+from airflow.hooks.http_hook import HttpHook
+import json
+
+
+class ElasticHook(HttpHook):
+    def search(self, index_and_type, args):
+        session = self.get_conn({})
+
+        url = self.base_url + index_and_type + '/_search'
+
+        req = requests.Request('GET', url, json=args)
+        prep_req = session.prepare_request(req)
+
+        resp = session.send(prep_req)
+        try:
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError:
+            logging.error("HTTP error: " + resp.reason)
+            raise AirflowException(str(resp.status_code) + ":" + resp.reason)
+
+        return json.loads(resp.content)
